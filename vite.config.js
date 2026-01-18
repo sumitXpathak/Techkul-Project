ab0c8b42f92d5089fa08770319cc49a064fdb33a
SumitXPathak <sumitpathak219@gmail.com>
2026-01-18 20:09:40 +0530

    hdbcxsc sh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  base: '/Techno-College-Website/',
   plugins: [
     react({
       babel: {
