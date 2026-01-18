8c47d13700f0daebc34c2c49e8580676a1c3d943
SumitXPathak <sumitpathak219@gmail.com>
2026-01-18 21:43:09 +0530

    vcvc

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  Base: '/techkulproject/',
+  Base: '/Techkul-Project/',
   plugins: [
     react({
       babel: {
