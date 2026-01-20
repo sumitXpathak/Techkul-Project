0c04f1314b64e5893703a4ab5da058856e999461
SumitXPathak <sumitpathak219@gmail.com>
2026-01-20 17:03:37 +0530

    12112

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
-// CHANGE 1: Import HashRouter instead of BrowserRouter to fix GitHub Pages routing
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import ScrollToTop
+import ScrollToTop from './components/ScrollToTop'; 
+
 import Navbar from './components/Navbar';
-import Admissions from './Pages/Addmissions';
 import Footer from './components/Footer';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Admissions from './Pages/Addmissions';
 import Faculty from './Pages/Faculty';
 import Academics from './Pages/Academics';
 import Contact from './Pages/Contact';
-import VirtualTour from './Pages/Tour';
+import Tour from './Pages/Tour';
 
 const App = () => {
   return (
-    // CHANGE 2: Use Router (which is now HashRouter)
     <Router>
       <ScrollToTop />
       
+      {/* --- FIX IS HERE: Added dark:bg-slate-900 and dark:text-white --- */}
       <div className="font-sans text-gray-900 bg-white dark:bg-slate-900 dark:text-white flex flex-col min-h-screen transition-colors duration-300">
         <Navbar />
         
         <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/admissions" element={<Admissions />} />
-          <Route path="/faculty" element={<Faculty />} />
-          <Route path="/academics" element={<Academics />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/tour" element={<VirtualTour />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/faculty" element={<Faculty />} />
+            <Route path="/academics" element={<Academics />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/tour" element={<Tour />} />
+          </Routes>
         </div>
-        {/* Added Footer here so it appears on all pages */}
+
         <Footer />
       </div>
     </Router>
