6dd88cfeed2572a8543e6420c80f0d1672da14b1
SumitXPathak <sumitpathak219@gmail.com>
2026-01-20 19:50:41 +0530

    1211

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import ScrollToTop
 import ScrollToTop from './components/ScrollToTop'; 
